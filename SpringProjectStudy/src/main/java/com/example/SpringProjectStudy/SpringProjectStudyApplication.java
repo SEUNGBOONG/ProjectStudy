b38ecea37a5811938a9d74330198c7d21bda329b
승엽 <tmdduqflfl@naver.com>
2024-06-19 15:54:21 +0900

    Spring Project Study Setting : Project:Gradle Language: Java Spring Boot: 최신 버전 Project Metadata: Group, Artifact 등을 입력합니다. Dependencies: Spring Web, Spring Data JPA, H2 Database, Spring Boot DevTools, Lombok

---
+++ SpringProjectStudy/src/main/java/com/example/SpringProjectStudy/SpringProjectStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringProjectStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectStudyApplication.class, args);
+	}
+
+}
---
+++ SpringProjectStudy/src/test/java/com/example/SpringProjectStudy/SpringProjectStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringProjectStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
